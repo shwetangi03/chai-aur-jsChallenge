5942481dcec6ed7a1c8a7a2af9c31c5fda677d89
Shwetangi <shwetangichaurasia4@gmail.com>
2024-07-16 14:13:28 +0530

    Day-1

---
+++ 01_datatypes&Variables.js
@@ -0,0 +1,30 @@
+// task-1
+var a= 10
+console.log(a);
+
+// task-2
+let name="shwetangi"
+console.log(name);
+
+// task-3
+const isLoggedin= false
+console.log(isLoggedin);
+
+// task-4
+let arr=[2,4,5,6,4]
+console.log(arr);
+
+console.log(typeof a);
+console.log(typeof name);
+console.log(typeof isLoggedin);
+console.log(typeof arr);
+
+// task-5
+let num= 10;
+console.log(num);
+num= 20
+console.log(num);
+
+// task-6
+const str="shwee"
+str="shwetangi"  //typeerror because we cant reassign to constant
