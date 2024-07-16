fac4ac26d7307c52f52006b744a4fef3dd757b70
Shwetangi <shwetangichaurasia4@gmail.com>
2024-07-16 14:38:14 +0530

    02_Operator

---
+++ 02_Operators.js
@@ -0,0 +1,45 @@
+// task-1
+let a = 10;
+let b = 5;
+console.log(a + b);
+
+// task-2
+console.log(a - b);
+
+// task-3
+console.log(a * b);
+
+// task-4
+console.log(a / b);
+
+// task-5
+console.log(a % b);
+
+// task-6
+console.log((a += b));
+
+// task-7
+console.log((a -= b));
+
+// task-8
+console.log(a > b);
+console.log(a < b);
+
+// task-9
+console.log(a >= b);
+console.log(a <= b);
+
+// task-10
+console.log(a == b);
+console.log(a === b);
+
+// task-11
+let res = a === 0 && b === 10;
+console.log(res);
+
+// task-12
+let result = a === 0 && b === 10;
+console.log(result);
+
+// task-13
+console.log(a != b);
